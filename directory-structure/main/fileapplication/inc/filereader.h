56ba824048986bd6ea82de02e212fc5f3ecddf68
christopher-preschern <101522135+christopher-preschern@users.noreply.github.com>
2022-03-13 19:32:53 +0100

    Create filereader.h

---
+++ directory-structure/main/fileapplication/inc/filereader.h
@@ -0,0 +1,6 @@
+#ifndef FILEREADER_H
+#define FILEREADER_H
+/* Reads the content of a file and stores it in the provided "buffer"
+   if is is long enough according to its provided "length" */
+void getFileContent(char* buffer, int length);
+#endif
