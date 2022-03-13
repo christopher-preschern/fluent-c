5a289a8b0ba62f0222df9855c42a5a80146a5489
christopher-preschern <101522135+christopher-preschern@users.noreply.github.com>
2022-03-13 19:36:16 +0100

    Create pseudorandom.h

---
+++ directory-structure/main/randomapplication/inc/pseudorandom.h
@@ -0,0 +1,5 @@
+#ifndef PSEUDORANDOM_H
+#define PSEUDORANDOM_H
+/* Returns a pseudo random number lower than the provided maximum number (parameter `max')*/
+unsigned int getRandomNumber(int max);
+#endif
