60e34ac9e2f90f94a1edd3d76c4474baab4ab65c
christopher-preschern <101522135+christopher-preschern@users.noreply.github.com>
2022-03-16 12:17:23 +0100

    Delete bernsteinhash.h

--- directory-structure/main/hashlibrary/bernsteinhash.h
+++
@@ -1,6 +0,0 @@
-#ifndef BERSTEINHASH_H
-#define BERNSTEINHASH_H
-/* Returns the hash value of the provided "buffer" of size "length".
-   The hash is calculated according to the D.J. Bernstein algorithm. */
-unsigned int bernsteinHash(const char* buffer, int length);
-#endif
