9d398768850e754bd85c497ab7a0cffd732747c6
christopher-preschern <101522135+christopher-preschern@users.noreply.github.com>
2022-03-31 19:30:32 +0200

    Update callbackIterator.c

--- iterator/callbackIterator.c
+++ iterator/callbackIterator.c
@@ -10,28 +10,47 @@ struct ACCOUNT
 {
   char loginname[MAX_NAME_LENGTH];
   char password[MAX_PWD_LENGTH];
-  int nextPosition;
+  struct ACCOUNT* next;
 };
 
+/* for simplicity reasons, we'll have a fixed set of list elements */
+static struct ACCOUNT accountData1;
+static struct ACCOUNT accountData2;
+static struct ACCOUNT accountData3;
 
-/* for simplicity reasons, we don't have a linked list with pointers, but static index info */
-static struct ACCOUNT accountData[MAX_USERS] = {{"Y","Y",1}, {"B","B",2}, {"C","C",3}, {"X","X",4}, {"Y","Y",0}};
+static void initList()
+{
+  strcpy(accountData1.loginname, "A");
+  strcpy(accountData1.password, "A");
+  accountData1.next = &accountData2;
+  
+  strcpy(accountData2.loginname, "B");
+  strcpy(accountData2.password, "B");
+  accountData2.next = &accountData3;
+  
+  strcpy(accountData3.loginname, "C");
+  strcpy(accountData3.password, "C");
+  accountData3.next = NULL;
+}
+
+
+[MAX_USERS] = {{"Y","Y",1}, {"B","B",2}, {"C","C",3}, {"X","X",4}, {"Y","Y",0}};
+
+static struct ACCOUNT* getFirst(struct ACCOUNT* current)
+{
+  return &accountData1;
+}
 
 static struct ACCOUNT* getNext(struct ACCOUNT* current)
 {
-  if(current->nextPosition == 0)
-  {
-    return NULL;
-  }
-  else
-  {
-    return &accountData[current->nextPosition];
-  }
+  return current->nextPosition;
 }
 
 
 void iterateLoginNames(FP_CALLBACK callback, void* arg)
 {
+  initList();
+  
   struct ACCOUNT* element = &accountData[0];
   while(element != NULL)
   {
