0bbd9e0305d84c3912fa66314e94a66acd6b388a
christopher-preschern <101522135+christopher-preschern@users.noreply.github.com>
2022-04-06 19:53:13 +0200

    Update cursorIterator.c

--- iterator/cursorIterator.c
+++ iterator/cursorIterator.c
@@ -19,6 +19,11 @@ struct ACCOUNT
 /* for simplicity reasons, we don't have a linked list with pointers, but static index info */
 static struct ACCOUNT accountData[MAX_USERS] = {{"A","A",1}, {"B","B",2}, {"C","C",3}, {"X","X",4}, {"Y","Y",0}};
 
+static struct ACCOUNT* getFirst()
+{
+    return &accoundData[0];
+}
+
 static struct ACCOUNT* getNext(struct ACCOUNT* current)
 {
   if(current->nextPosition == 0)
@@ -32,40 +37,29 @@ static struct ACCOUNT* getNext(struct ACCOUNT* current)
 }
 
 
-
-struct Iterator
+struct ITERATOR* createIterator()
 {
-  char buffer[MAX_NAME_LENGTH];
-  struct ACCOUNT* element;
-};
-
-
-ITERATOR_HANDLE createIterator()
-{
-  struct Iterator* iterator = malloc(sizeof(struct Iterator));
-  iterator->element = &accountData[0];
+  struct ITERATOR* iterator = malloc(sizeof(struct ITERATOR));
+  iterator->element = getFirst();
   return iterator;
 }
 
-
-void destroyIterator(ITERATOR_HANDLE iterator)
+char* getNextLoginName(struct ITERATOR* iterator)
 {
-  free(iterator);
-}
-
-
-
-char* getNextLoginName(ITERATOR_HANDLE iterator)
-{
-  iterator->element = getNext(iterator->element);
   if(iterator->element != NULL)
   {
     strcpy(iterator->buffer, iterator->element->loginname);
+    iterator->element = getNext(iterator->element);
     return iterator->buffer;
   }
   else
   {
     return NULL;
   }
+}
+
+void destroyIterator(struct ITERATOR* iterator)
+{
+  free(iterator);
 }
 
