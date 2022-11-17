71e57505f8fe7876e2addf03510a7fbeb7c0215c
christopher-preschern <101522135+christopher-preschern@users.noreply.github.com>
2022-11-17 19:26:37 +0100

    Update main.c

--- CVu/main.c
+++ CVu/main.c
@@ -6,17 +6,30 @@
   #include <windows.h>
 #endif
 
+
+void getDirectoryName(char* dirname)
+{
+  #ifdef __unix__
+    sprintf(dirname, "%s%s", getenv("HOME"), "/newdir/");
+  #elif defined _WIN32
+    sprintf(dirname, "%s%s%s", getenv("HOMEDRIVE"), getenv("HOMEPATH"),
+    "\\newdir\\");
+  #endif
+}
+
+void createNewDirectory(char* dirname)
+{
+  #ifdef __unix__
+    mkdir(dirname,S_IRWXU);
+  #elif defined _WIN32
+    CreateDirectory (dirname, NULL);
+  #endif
+}
+
 int main()
 {
   char dirname[50];
-  #ifdef __unix__
-    sprintf(dirname, "%s%s", getenv("HOME"), "/newdir/");
-    printf("DIRNAME: %s\n", dirname);
-    mkdir(dirname,S_IRWXU);
-  #elif defined _WIN32
-    sprintf(dirname, "%s%s%s", getenv("HOMEDRIVE"), getenv("HOMEPATH"),
-      "\\newdir\\");
-    CreateDirectory(dirname, NULL);
-  #endif
+  getDirectoryName(dirname);
+  createNewDirectory(dirname);
   return 0;
 }
