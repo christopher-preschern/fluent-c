31a9e4d8d50793fa342688703c9d6f805924a826
christopher-preschern <101522135+christopher-preschern@users.noreply.github.com>
2022-11-17 18:39:20 +0100

    Create main.c

---
+++ CVu/main.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <stdlib.h>
+#ifdef __unix__
+  #include <sys/stat.h>
+#elif defined _WIN32
+  #include <windows.h>
+#endif
+
+int main()
+{
+  char dirname[50];
+  #ifdef __unix__
+    sprintf(dirname, "%s%s", getenv("HOME"), "/newdir/");
+    mkdir(dirname,S_IRWXU);
+  #elif defined _WIN32
+    sprintf(dirname, "%s%s%s", getenv("HOMEDRIVE"), getenv("HOMEPATH"),
+      "\\newdir\\");
+    CreateDirectory(dirname, NULL);
+  #endif
+  return 0;
+}
